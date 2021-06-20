9d8fffa25151fba1af6dc5340afc2a2189115893
praveen-kharb <62607338+praveen-kharb@users.noreply.github.com>
2021-06-20 22:10:46 +0530

    Delete covid-helpdesk-server-main directory

--- covid-helpdesk-server-main/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const submit_data = require("../controllers/request/submit_data");
-const router = express.Router();
-
-
-router.post('/submit', submit_data);
-
-
-module.exports = router;
